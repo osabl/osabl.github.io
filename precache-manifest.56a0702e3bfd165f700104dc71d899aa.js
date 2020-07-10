0ba12857b49b2fec17b71f8f375f32beb1b888b9
osabl <andrii.osabliuk@gmail.com>
2020-07-10 20:36:24 +0300

    init

---
+++ precache-manifest.56a0702e3bfd165f700104dc71d899aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2e5a511cd989fba597",
+    "url": "/css/chunk-vendors.bdc31e3b.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "2b18493df2c61be567cee2897f96c1bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd9cece7ce895c9e773a",
+    "url": "/js/app.db9db5dd.js"
+  },
+  {
+    "revision": "7b2e5a511cd989fba597",
+    "url": "/js/chunk-vendors.12c21499.js"
+  },
+  {
+    "revision": "5da060d3e0249ff045f23358833ee0df",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
